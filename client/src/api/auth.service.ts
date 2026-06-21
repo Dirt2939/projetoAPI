3114d425723ab51fd48758954680a46bae037baa
Dirt2939 <rafaelhunemeier@gmail.com>
2026-06-21 00:45:17 -0300

    refactor(api): Adds an Axios interceptor for automatic token sending

--- client/src/api/auth.service.ts
+++ client/src/api/auth.service.ts
@@ -1,4 +1,4 @@
-import { api } from "./axios";
+import api  from "./axios";
 
 export async function login(email: string, password: string) {
   try {
--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,5 +1,32 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: "http://localhost:3030",
+declare module 'axios' {
+  export interface AxiosRequestConfig {
+    skipAuth?: boolean;
+  }
+  export interface InternalAxiosRequestConfig {
+    skipAuth?: boolean;
+  }
+}
+
+const api = axios.create({
+  baseURL: 'http://localhost:3030'
 });
+
+api.interceptors.request.use(
+  (config) => {
+    if (config.skipAuth) {
+      return config;
+    }
+
+    const token = localStorage.getItem('token');
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
