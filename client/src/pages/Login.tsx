77f12dd6caf0d41c4c61022c0bc9a1ae856b5e95
Dirt2939 <rafaelhunemeier@gmail.com>
2026-06-20 23:22:38 -0300

    feat(auth): persist user after login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { login } from "../api/auth.service";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [isChecked, setIsChecked] = useState(false);
   const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState(false);
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     if (!email || email.trim() === "" || !password || password.trim() === "") {
       // Poupar o banco
-      setError("Campo(s) inválidos");
+      setError(true);
       return;
     }
 
@@ -23,11 +23,14 @@ function Login() {
       const res = await login(email, password);
 
       if (res.status != 200) {
-        setError("Credenciais Incorretas.");
+        setError(true);
         return;
       }
+
+      localStorage.setItem("user", JSON.stringify(res.data))
+
     } catch (error) {
-      setError("Algo deu errado.");
+      setError(true);
       console.log(error);
       return;
     }
@@ -38,6 +41,14 @@ function Login() {
   const handleCheckbox = () => {
     setIsChecked((prev) => !prev);
   };
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError(false);
+      }, 3000);
+    }
+  }, [error]);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-zinc-950 px-4">
@@ -54,20 +65,7 @@ function Login() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Email"
-              className="
-            w-full
-            px-4 py-3
-            rounded-xl
-            bg-zinc-800
-            border border-zinc-700
-            text-white
-            placeholder:text-zinc-500
-            focus:outline-none
-            focus:border-violet-500
-            focus:ring-4
-            focus:ring-violet-500/20
-            transition
-          "
+              className={`${error ? "border-red-500" : "border-zinc-700"} w-full px-4 py-3 rounded-xl bg-zinc-800 border text-white placeholder:text-zinc-500 focus:outline-none focus:border-violet-500 focus:ring-4 focus:ring-violet-500/20 transition`}
             />
           </div>
 
@@ -77,37 +75,33 @@ function Login() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Senha"
-              className="
-            w-full
-            px-4 py-3
-            rounded-xl
-            bg-zinc-800
-            border border-zinc-700
-            text-white
-            placeholder:text-zinc-500
-            focus:outline-none
-            focus:border-violet-500
-            focus:ring-4
-            focus:ring-violet-500/20
-            transition
-          "
+              className={`${error ? "border-red-500" : "border-zinc-700"} w-full px-4 py-3 rounded-xl bg-zinc-800 border text-white placeholder:text-zinc-500 focus:outline-none focus:border-violet-500 focus:ring-4 focus:ring-violet-500/20 transition`}
             />
           </div>
 
           <label className="flex size-fit items-center justify-start gap-2 text-sm text-zinc-400 select-none hover:cursor-pointer p-1.5">
-
             <button
               type="button"
               onClick={handleCheckbox}
-              className={`w-11 h-6 rounded-full p-1 transition flex items-center ${isChecked ? "bg-violet-600" : "bg-zinc-700"} hover:cursor-pointer`}>
-              <div className={`w-4 h-4 bg-white rounded-full transition-transform ${isChecked ? "translate-x-5" : "translate-x-0"}`}/>
+              className={`w-11 h-6 rounded-full p-1 transition flex items-center ${isChecked ? "bg-violet-600" : "bg-zinc-700"} hover:cursor-pointer`}
+            >
+              <div
+                className={`w-4 h-4 bg-white rounded-full transition-transform ${isChecked ? "translate-x-5" : "translate-x-0"}`}
+              />
             </button>
-                <span>Mostrar senha</span>
+            <span>Mostrar senha</span>
           </label>
+
+          {error && (
+            <h3 className="text-red-500 text-center">
+              Credenciais incorretas.
+            </h3>
+          )}
 
           <button
             type="submit"
-            className="w-full py-3 rounded-xl bg-violet-600 text-white font-medium hover:bg-violet-500 hover:scale-105 active:scale-[0.98] transition cursor-pointer">
+            className="w-full py-3 rounded-xl bg-violet-600 text-white font-medium hover:bg-violet-500 hover:scale-105 active:scale-[0.98] transition cursor-pointer"
+          >
             Entrar
           </button>
         </form>
